cefacf235a2841ba1596373372d1d0e66bb4e07b
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-04-19 09:50:12 -0400

    about section completed

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -6,6 +6,8 @@ import { connect } from 'react-redux';
 
 import { setPage } from '../redux/action';
 
+import PizzaIcon from '../images/Pizza_icon_white.png';
+
 class Home extends React.Component{
     componentWillMount(){
         this.props.dispatch(setPage('Home'));
@@ -15,27 +17,7 @@ class Home extends React.Component{
         return(
             <div>
                 <Hero />
-                <div>
-                    <h2>About Us</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur</p>
-                    <div>
-                        <div>
-                            <img src="" alt=""/>
-                            <span>Great Pizza</span>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-                        </div>
-                        <div>
-                            <img src="" alt=""/>
-                            <span>Great Pizza</span>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-                        </div>
-                        <div>
-                            <img src="" alt=""/>
-                            <span>Great Pizza</span>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-                        </div>
-                    </div>
-                </div>
+                <About />
             </div>
         )
     }
@@ -43,13 +25,39 @@ class Home extends React.Component{
 
 const Hero = () => {
     return(
-        <div className={s.hero}>
+        <section className={s.hero}>
             <div className={s.darken}>
                 <Navbar />
                 <p className={s.heroTitle}>Best Pizza in Town</p>
                 <button className={s.heroButton}>View Menu</button>
             </div>
-        </div>
+        </section>
+    )
+}
+
+const About = () => {
+    return(
+        <section className={s.about}>
+            <h2 className={s.aboutTitle}>About Us</h2>
+            <p className={s.aboutDesc} >Lorem ipsum dolor sit amet, consectetur</p>
+            <div className={s.flexContainer} >
+                <div className={s.AboutUnits}>
+                    <img className={s.aboutUnitImg} src={PizzaIcon} alt="Pizza Icon"/>
+                    <span className={s.aboutUnitTitle} >Great Pizza</span>
+                    <p className={s.aboutUnitDesc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                </div>
+                <div className={s.AboutUnits}>
+                    <img className={s.aboutUnitImg} src={PizzaIcon} alt="Pizza Icon"/>
+                    <span className={s.aboutUnitTitle}>Great Pizza</span>
+                    <p className={s.aboutUnitDesc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                </div>
+                <div className={s.AboutUnits}>
+                    <img className={s.aboutUnitImg} src={PizzaIcon} alt="Pizza Icon"/>
+                    <span className={s.aboutUnitTitle}>Great Pizza</span>
+                    <p className={s.aboutUnitDesc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                </div>
+            </div>
+        </section>
     )
 }
 
