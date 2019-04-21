d98b7c1c7943a13fe0e1161ff2c2d6903cf13ac6
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-04-21 18:42:52 -0400

    separated footer into componet for easyier development

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -13,46 +13,57 @@ const Footer = () => {
     return(
         <footer className={s.footer}>
             <div className={s.container}>
-                <div className={s.opening}>
-                    <h4 className={s.title}>Opening Hours</h4>
-                    <div className={s.hours}>
-                        <div className={s.hoursSection}>
-                            <p className={s.hoursTitle}>Monday - Friday</p>
-                            <p className={s.hoursText}>8.00 am - 10.00 pm</p>
-                        </div>
-                        <div className={s.hoursSection}>
-                            <p className={s.hoursTitle}>Saturday</p>
-                            <p className={s.hoursText}>10.00 am - 5.00 pm</p>
-                        </div>
-                        <div className={s.hoursSection}>
-                            <p className={s.hoursTitle}>Sunday</p>
-                            <p className={s.hoursText}>Closed</p>
-                        </div>
-                    </div>
-                </div>
-                <div className={s.contact}>
-                    <h4 className={s.title}>Contact Us</h4>
-                    <div className={s.contactSocialIconContainer}>
-                        <img className={s.contactSocialIcon} src={twitterIcon} alt="Social Media Icon"/>
-                        <img className={s.contactSocialIcon} src={facebookIcon} alt="Social Media Icon"/>
-                        <img className={s.contactSocialIcon} src={instagramIcon} alt="Social Media Icon"/>
-                    </div>
-                    <div className={s.contactSection}>
-                        <img className={s.contactSectionIcon} src={locationIcon} alt="Location Icon"/>
-                        <p className={s.contactSectionText}>203 Fake St. Mountain View, San Francisco, California, USA</p>
-                    </div>
-                    <div className={s.contactSection}>
-                        <img className={s.contactSectionIcon} src={phoneIcon} alt="Phone Icon"/>
-                        <p className={s.contactSectionText}>+1 978-236-7889</p>
-                    </div>
-                    <div className={s.contactSection}>
-                        <img className={s.contactSectionIcon} src={emailIcon} alt="Email Icon"/>
-                        <p className={s.contactSectionText}>JacquezWilliams@teleworm.us</p>
-                    </div>
-                </div>
+                <Opening />
+                <Contact />
             </div>
         </footer> 
     )
 }
 
+const Opening = () => {
+    return(
+        <div className={s.opening}>
+                <h4 className={s.title}>Opening Hours</h4>
+                <div className={s.hours}>
+                    <div className={s.hoursSection}>
+                        <p className={s.hoursTitle}>Monday - Friday</p>
+                        <p className={s.hoursText}>8.00 am - 10.00 pm</p>
+                    </div>
+                    <div className={s.hoursSection}>
+                        <p className={s.hoursTitle}>Saturday</p>
+                        <p className={s.hoursText}>10.00 am - 5.00 pm</p>
+                    </div>
+                    <div className={s.hoursSection}>
+                        <p className={s.hoursTitle}>Sunday</p>
+                        <p className={s.hoursText}>Closed</p>
+                    </div>
+                </div>
+            </div>
+    )
+}
+
+const Contact = () => {
+    return(
+        <div className={s.contact}>
+            <h4 className={s.title}>Contact Us</h4>
+            <div className={s.contactSocialIconContainer}>
+                <img className={s.contactSocialIcon} src={twitterIcon} alt="Social Media Icon"/>
+                <img className={s.contactSocialIcon} src={facebookIcon} alt="Social Media Icon"/>
+                <img className={s.contactSocialIcon} src={instagramIcon} alt="Social Media Icon"/>
+            </div>
+            <div className={s.contactSection}>
+                <img className={s.contactSectionIcon} src={locationIcon} alt="Location Icon"/>
+                <p className={s.contactSectionText}>203 Fake St. Mountain View, San Francisco, California, USA</p>
+            </div>
+            <div className={s.contactSection}>
+                <img className={s.contactSectionIcon} src={phoneIcon} alt="Phone Icon"/>
+                <p className={s.contactSectionText}>+1 978-236-7889</p>
+            </div>
+            <div className={s.contactSection}>
+                <img className={s.contactSectionIcon} src={emailIcon} alt="Email Icon"/>
+                <p className={s.contactSectionText}>JacquezWilliams@teleworm.us</p>
+            </div>
+        </div>
+    )
+}
 export default Footer;
