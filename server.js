e075fd7a4ab0d5bd27dff91dff36992eef869c17
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-04-22 14:25:19 -0400

    setup for deploy

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
