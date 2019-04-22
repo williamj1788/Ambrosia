7b2242766f33b9ba5740d16054fa897d1904639b
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-04-22 09:39:15 -0400

    put HotDeal component into different files

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,7 +1,10 @@
 import React from 'react';
+
 import Navbar from './Navbar';
 import ReviewSection from './ReviewSection';
 import Footer from './Footer';
+import HotDeals from './HotDeals';
+
 import s from '../styles/Home.module.scss';
 
 import { connect } from 'react-redux';
@@ -86,50 +89,6 @@ export const Unit = ({ img, title, desc }) => {
             <img className={s.aboutUnitImg} src={img} alt="Icon"/>
             <span className={s.aboutUnitTitle} >{title}</span>
             <p className={s.aboutUnitDesc}>{desc}</p>
-        </div>
-    )
-}
-
-const HotDeals = () => {
-    return(
-        <section className={s.hotDeals}>
-            <h3 className={s.hotDealsTitle}>Hot Deals</h3>
-            <DealContainer />
-        </section>
-    )
-}
-
-const DealContainer = () => {
-    return(
-        <div className={s.dealsContainer}>
-            <Deal />
-            <Deal />
-            <Deal />
-            <Deal />
-            <Deal />
-            <Deal />
-            <Deal />
-            <Deal />
-            <Deal />
-            <Deal />
-        </div>
-    )
-}
-
-const Deal = () => {
-    return(
-        <div className={s.deal}>
-            <img className={s.dealImg} src={PizzaIcon} alt=""/>
-            <div className={s.dealInfo}>
-                <span className={s.dealTitle}>Pizza Title</span>
-                <p className={s.dealDesc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-                <div className={s.dealPriceContainer}>
-                    <span className={s.dealPrice}><s>10.99</s></span>
-                    <span className={s.dealPrice}>7.99</span>
-                    <button className={s.dealButton}>Place Order</button>
-                </div>
-            </div>
         </div>
     )
 }
