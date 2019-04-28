0802b482b0fc38923992baee4e910aa1a71b5f5d
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-04-28 17:19:31 -0400

    passwords are now hashed

--- userRouter.js
+++ userRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const User = require('./User');
 const secretKey = require('./config').secretKey;
@@ -11,32 +12,43 @@ router.get('/', verifyToken, (req, res) => {
 });
 
 router.post('/create', (req, res) => {
-    const newUser = new User({
-        email: req.body.Email,
-        password: req.body.Password,
-        firstname: req.body.Firstname,
-        lastname: req.body.Lastname,
-        ...(req.body.Address && {address: req.body.Address}),
-    });
-    newUser.save().then(user => {
-        jwt.sign({user}, secretKey, (err, token) => {
-            if(err) throw err;
-            res.json({token});
-        })
-    });
-});
-
-router.post('/login',(req, res) => {
-    User.findOne({email: req.body.Email, password: req.body.Password}, (err, user) => {
+    bcrypt.hash(req.body.Password, 10, (err, hash) => {
         if(err) throw err;
-        if(user){
+        const newUser = new User({
+            email: req.body.Email,
+            password: hash,
+            firstname: req.body.Firstname,
+            lastname: req.body.Lastname,
+            ...(req.body.Address && {address: req.body.Address}),
+        });
+        newUser.save().then(user => {
             jwt.sign({user}, secretKey, (err, token) => {
                 if(err) throw err;
                 res.json({token});
             })
+        });
+    });
+});
+
+router.post('/login',(req, res) => {
+    User.findOne({email: req.body.Email}, (err, user) => {
+        if(err) throw err;
+        if(user){
+            bcrypt.compare(req.body.Password, user.password, (err, isValidUser) => {
+                if(isValidUser){
+                    jwt.sign({user}, secretKey, (err, token) => {
+                        if(err) throw err;
+                        return res.json({token});
+                    })
+                }else{
+                    res.status(404).json({
+                        error: 'Email or Password is incorrect'
+                    });
+                }
+            });
         }else{
             res.status(404).json({
-                error: 'Username or Email is incorrect'
+                error: 'Email or Password is incorrect'
             });
         }
     });
