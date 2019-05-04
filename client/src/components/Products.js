657db5ac6dd247f7fdc9a0185a1a160fb0f80b07
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-05-04 14:24:34 -0400

    finish product elements

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import s from '../styles/Products.module.scss';
+import { FaTrashAlt } from "react-icons/fa";
 
 class Products extends React.Component{
     
@@ -35,15 +36,31 @@ class Products extends React.Component{
                 <Navbar />
                 <h1 className={s.title}>Products</h1>
                 <button className={s.createButton} type="button">Create A Product</button>
-                <input className={s.searchBar} type="text"/>
-                <div>
-                    <button type="button">
-                        FakePizza
-                    </button>
+                <input className={s.searchBar} type="text" placeholder="Search for prouduct" />
+                <div className={s.productContainer} >
+                    <Product name="FakePizza" type="pizza" />
+                    <Product name="FakePizza" type="pizza" />
+                    <Product name="FakePizza" type="pizza" />
+                    <Product name="FakePizza" type="pizza" />
+                    <Product name="FakePizza" type="pizza" />
+                    <Product name="FakePizza" type="pizza" />
                 </div>
             </div>
         )
     }
+}
+
+const Product = ({name, type}) => {
+    return(
+        <button className={s.product} type="button">
+            <div className={s.icon}>
+                <FaTrashAlt 
+                size="1.5em"
+                />
+            </div>
+            <span>{`${name} - ${type}`}</span>
+        </button>
+    )
 }
 
 export default connect(state => {
