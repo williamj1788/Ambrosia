1f4b7c1a866abd5f4dbd033c1afe001c97ae5d70
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-05-04 17:58:36 -0400

    created create product form

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -9,6 +9,7 @@ class Products extends React.Component{
     
     state = {
         redirect: false,
+        filename: 'Upload image',
     }
     
     componentDidMount(){
@@ -24,10 +25,19 @@ class Products extends React.Component{
         //     });
         // }
     }
-    
+
+    clickFileInput = () =>{
+        document.querySelector('input[name=picture]').click();
+    }
+    handleFileChange = event => {
+        this.setState({
+            filename: event.target.value.split('\\').pop()
+        })
+    }
     
     render(){
-        if(this.state.redirect){
+        const { redirect, filename } = this.state
+        if(redirect){
             return <Redirect to='/' />
         }
 
@@ -50,7 +60,38 @@ class Products extends React.Component{
                         <div className={s.header}>
                             <span>FakePizza</span>
                         </div>
-                        
+                        <form className={s.form}>
+                            <div className={s.formRecord}>
+                                <label className={s.label} htmlFor="type">Type:</label>
+                                <select className={s.input} name="type">
+                                    <option value="pizza">Pizza</option>
+                                    <option value="pasta">pasta</option>
+                                    <option value="bread">Bread</option>
+                                    <option value="desserts">Desserts</option>
+                                    <option value="drinks">Drinks</option>
+                                </select>
+                            </div>
+                            <div className={s.formRecord}>
+                                <label className={s.label} htmlFor="picture">Image:</label>
+                                <input onChange={this.handleFileChange} className={s.file} type="file" name="picture" required />
+                                <button onClick={this.clickFileInput} type='button' className={s.fileButton}>{filename}</button>
+                            </div>
+                            <div className={s.formRecord}>
+                                <label className={s.label} htmlFor="name">Name:</label>
+                                <input className={s.input} type="text" name="name" required />
+                            </div>
+                            <div className={s.formRecord}>
+                                <label className={s.label} htmlFor="description">Desc:</label>
+                                <textarea className={s.input} style={{height: '200px'}} name="description" cols="30" rows="10" placeholder="30 word limit" required ></textarea>
+                            </div>
+                            <div className={s.formRecord}>
+                                <label className={s.label} htmlFor="price">Price:</label>
+                                <input className={s.input} minLength="0" step='0.01' type="number" name="price" required />
+                            </div>
+                            <div className={s.formRecord}>
+                                <button className={s.submitButton} type="submit">Create</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -60,14 +101,19 @@ class Products extends React.Component{
 
 const Product = ({name, type}) => {
     return(
-        <button className={s.product} type="button">
-            <div className={s.icon}>
-                <FaTrashAlt 
-                size="1.5em"
-                />
-            </div>
-            <span>{`${name} - ${type}`}</span>
-        </button>
+        <div>
+            <button className={s.product} type="button">
+                <div className={s.icon}>
+                    <FaTrashAlt 
+                    size="1.5em"
+                    />
+                </div>
+                <span>{`${name} - ${type}`}</span>
+            </button>
+            <button className={s.deal}>
+                <span> Click to add Deal</span>
+            </button>
+        </div>
     )
 }
 
