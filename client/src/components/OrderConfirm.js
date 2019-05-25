ad641f0ba191dfebb4fa59025bfac2877871033b
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-05-25 18:38:47 -0400

    orderconfirm page is now responsive

--- client/src/components/OrderConfirm.js
+++ client/src/components/OrderConfirm.js
@@ -11,10 +11,25 @@ class OrderConfirm extends React.Component{
     state = {
         address: this.props.user && this.props.user.address,
         center: false,
+        mapHieght: window.innerWidth > 1980 ? 900 : window.innerHeight - 140 > 600 ? 600 : window.innerHeight - 140,
+
     }
 
     componentDidMount(){
         this.refs.geo.focus();
+        window.addEventListener('resize', this.handleResize);
+    }
+    componentWillUnmount() {;
+        window.removeEventListener('resize', this.handleResize);
+    }
+
+    handleResize = () => {
+        const newHieght =  window.innerWidth > 1980 ? 900 : window.innerHeight - 140 > 600 ? 600 : window.innerHeight - 140;
+        if(this.state.mapHieght !== newHieght){
+            this.setState({
+                mapHieght: newHieght
+            });
+        }
     }
 
     handleSuggest = suggest => {
@@ -24,33 +39,34 @@ class OrderConfirm extends React.Component{
     }
 
     render(){
-        const { address, center } = this.state;
+        const { address, center, mapHieght } = this.state;
         return(
             <div>
                 <Navbar />
-                <div className={s.flexContainer}>
-                    <Geosuggest
-                    autoActivateFirstSuggest
-                    className={s.geo}
-                    country='us'
-                    placeholder='Type address here'
-                    initialValue={address}
-                    queryDelay={500}
-                    ref='geo'
-                    onSuggestSelect={this.handleSuggest} />
-                    <button className={s.button} type='button' >Confirm Address</button>
-                </div>
-                
-                <div className={s.map}>
-                    <GoogleMapReact
-                    bootstrapURLKeys={{ key: keys.googleID }}
-                    defaultCenter={{lat: 35.0527, lng: -78.8784 }}
-                    center={center}
-                    defaultZoom={11}
-                    zoom={center && 18}
-                    options={{ minZoom: 11 }}>
-                        {center && <Marker lat={center.lat} lng={center.lng} text='Home' />}
-                    </GoogleMapReact>
+                <div className={s.content}>
+                    <div className={s.flexContainer}>
+                        <Geosuggest
+                        autoActivateFirstSuggest
+                        className={s.geo}
+                        country='us'
+                        placeholder='Type address here'
+                        initialValue={address}
+                        queryDelay={500}
+                        ref='geo'
+                        onSuggestSelect={this.handleSuggest} />
+                        <button className={s.button} type='button' >Confirm Address</button>
+                    </div>
+                    <div className={s.map} style={{height: mapHieght}} >
+                        <GoogleMapReact
+                        bootstrapURLKeys={{ key: keys.googleID }}
+                        defaultCenter={{lat: 35.0527, lng: -78.8784 }}
+                        center={center}
+                        defaultZoom={11}
+                        zoom={center && 18}
+                        options={{ minZoom: 11 }}>
+                            {center && <Marker lat={center.lat} lng={center.lng} />}
+                        </GoogleMapReact>
+                    </div>
                 </div>
             </div>
         )
