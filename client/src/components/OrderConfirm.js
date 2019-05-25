13e1617d9fbe3cbac736fae7724a5cf9639f9aec
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-05-25 17:14:53 -0400

    completed map and suggests

--- client/src/components/OrderConfirm.js
+++ client/src/components/OrderConfirm.js
@@ -5,31 +5,62 @@ import { connect } from 'react-redux';
 import GoogleMapReact from 'google-map-react';
 import s from '../styles/OrderConfirm.module.scss';
 import '../styles/geosuggest.scss'
+import keys from '../key';
 
 class OrderConfirm extends React.Component{
     state = {
         address: this.props.user && this.props.user.address,
+        center: false,
     }
 
     componentDidMount(){
-        this.refs.geo.update(this.props.user && this.props.user.address);
+        this.refs.geo.focus();
+    }
+
+    handleSuggest = suggest => {
+        this.setState({
+            center: suggest.location
+        });
     }
 
     render(){
+        const { address, center } = this.state;
         return(
             <div>
                 <Navbar />
-                <Geosuggest
-                autoActivateFirstSuggest
-                className={s.geo}
-                country='us'
-                placeholder='Type address here'
-                initialValue={this.state.address}
-                queryDelay={500}
-                ref='geo'/>
+                <div className={s.flexContainer}>
+                    <Geosuggest
+                    autoActivateFirstSuggest
+                    className={s.geo}
+                    country='us'
+                    placeholder='Type address here'
+                    initialValue={address}
+                    queryDelay={500}
+                    ref='geo'
+                    onSuggestSelect={this.handleSuggest} />
+                    <button className={s.button} type='button' >Confirm Address</button>
+                </div>
+                
+                <div className={s.map}>
+                    <GoogleMapReact
+                    bootstrapURLKeys={{ key: keys.googleID }}
+                    defaultCenter={{lat: 35.0527, lng: -78.8784 }}
+                    center={center}
+                    defaultZoom={11}
+                    zoom={center && 18}
+                    options={{ minZoom: 11 }}>
+                        {center && <Marker lat={center.lat} lng={center.lng} text='Home' />}
+                    </GoogleMapReact>
+                </div>
             </div>
         )
     }
+}
+
+const Marker = ({lat, lng}) => {
+    return(
+        <div className={s.marker} lat={lat} lng={lng}/>
+    )
 }
 
 export default connect(state => {
--- client/src/key.js
+++ client/src/key.js
@@ -1,1 +1,5 @@
-export default { clientID: '1064409062816-te616f091t5s0vh9mgnkacur1oqrqpr8.apps.googleusercontent.com'}
+export default { 
+    clientID: '1064409062816-te616f091t5s0vh9mgnkacur1oqrqpr8.apps.googleusercontent.com',
+    googleID: 'AIzaSyD4Vk1pihqYG7Ex-mRffyQLqnx6rDoLS-I'
+
+}
