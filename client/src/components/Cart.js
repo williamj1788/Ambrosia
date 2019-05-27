2864de17f2b2d79cd91e7060e99fa04bb28c93eb
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-05-27 13:47:46 -0400

    cart modal is now responsive

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -150,8 +150,8 @@ const Order = ({ name, price, qty, id, remove, edit }) => {
         <div className={s.order}>
             <div className={s.orderInfo}>
                 <p className={s.orderTitle}>{name}</p>
-                <p className={s.orderPrice}>{price}</p>
-                <select onChange={event => edit(id, event.target.value)} defaultValue={qty} name="Qty">
+                <p className={s.orderPrice}>{'$' + price.toFixed(2)}</p>
+                <select className={s.select} onChange={event => edit(id, event.target.value)} defaultValue={qty} name="Qty">
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
